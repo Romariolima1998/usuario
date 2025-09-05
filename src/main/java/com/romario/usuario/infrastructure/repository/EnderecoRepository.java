503b32686ca3ee7a2e4e5749cfa96a2d109c4a03
Romario lima <romariolima1006@gmail.com>
2025-09-05 10:31:10 -0300

    adicionado security

---
+++ src/main/java/com/romario/usuario/infrastructure/repository/EnderecoRepository.java
@@ -0,0 +1,10 @@
+package com.romario.usuario.infrastructure.repository;
+
+
+import com.romario.usuario.infrastructure.entity.Endereco;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EnderecoRepository extends JpaRepository<Endereco, Long> {
+}
